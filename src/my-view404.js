4cdfa22c63500aa1a647c2b95538a801cbe968e7
mufc1427 <isaaclobato@csus.edu>
2019-11-27 20:00:34 -0800

    Added sad emoji and more styles including mdeia queries for smaller devices

--- src/my-view404.js
+++ src/my-view404.js
@@ -9,10 +9,62 @@ class MyView404 extends PolymerElement {
           display: block;
 
           padding: 10px 20px;
+
+
+         
+
+          
         }
+
+         .flex-cont
+          {
+            display:flex;
+            justify-content:flex-start;
+            font-size:20pt;
+          }
+
+          #message
+          {
+            margin-top:40%;
+          }
+
+
+          @media screen and (max-width: 575px) {
+  .flex-cont {
+    display: block;
+
+  }
+
+    #message
+          {
+            margin-top:-25%;
+          }
+
+
+          #sad-emoji
+          {
+            font-size:268px !important;
+          }
+}
       </style>
 
+
+
+      <div class="flex-cont">
+      <section class="layout-item">
+      <span id="sad-emoji" style = 'font-size:300px;'> &#128542;</span>
+      </section>
+
+
+      <section class="layout-item">
+      <p id="message">
       Oops you hit a 404. <a href="[[rootPath]]">Head back to home.</a>
+     
+    
+      </p>
+       </section>
+
+      </div>
     `;
   }
 }
